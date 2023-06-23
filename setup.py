6d49c704ac059c70aa072f742ee42395532970e7
Akshay Paunikar <akshayakki29@gmail.com>
2023-06-23 12:04:50 +0530

    Initial Project setup done

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path:str)->List[str]:
+    """This function will return the list of requirements"""
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+            
+    return requirements
+
+setup(
+    name="MushroomClassification",
+    version="0.0.1",
+    author="Akshay Pandurang Paunikar",
+    author_email="akshayakki29@gmail.com",
+    packages=find_packages(),
+    install_requires = get_requirements("requirements.txt")
+)
