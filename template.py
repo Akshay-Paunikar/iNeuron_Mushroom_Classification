3d99e05bb1cc23c52c1090b3036317577be841d3
Akshay Paunikar <akshayakki29@gmail.com>
2023-06-23 12:24:04 +0530

    Initial Folder Structure Created using template

--- template.py
+++ template.py
@@ -1,1 +1,43 @@
 import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='[%(asctime)s]: %(message)s')
+
+project_name = "mushroom"
+
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/exception.py",
+    f"{project_name}/logger.py",
+    f"{project_name}/utils.py",
+    f"{project_name}/components/__init__.py"
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/pipeline/prediction_pipeline.py",
+    f"{project_name}/application/app.py",
+    f"{project_name}/application/templates/index.html",
+    f"{project_name}/application/templates/static/css/style.css",
+    f"{project_name}/application/static/css/style.css",
+    f"{project_name}/notebook/dataset/",
+    f"{project_name}/notebook/mushroom_EDA.ipynb",
+    f"{project_name}/notebook/mushroom_Model.ipynb"    
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+        
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+            
+    else:
+        logging.info(f"{filename} already exists")
